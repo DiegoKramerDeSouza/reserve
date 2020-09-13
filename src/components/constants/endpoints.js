f7a822640d08d9bd210d10006629d4763e2c3c50
Diego Kramer <diego.kramer99@gmail.com>
2020-09-13 15:09:06 -0500

    Add path prop

--- src/components/constants/endpoints.js
+++ src/components/constants/endpoints.js
@@ -1,5 +1,6 @@
 
 const envType = "prod";
+const path="api/reservation/";
 const dev="http://localhost:8080/api/reservation/";
 const prod="https://iasd-reservation.azurewebsites.net/api/reservation/";
 
