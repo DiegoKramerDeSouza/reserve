8cfd1f8ec08717cb3e226b94a6ee43a1971e76c1
Diego Kramer <diego.kramer99@gmail.com>
2020-09-13 14:23:16 -0500

    Env updated

--- src/components/constants/endpoints.js
+++ src/components/constants/endpoints.js
@@ -1,4 +1,7 @@
 
-const envType = "prod"
-export const reservationApi = envType === "prod" ? process.env.REACT_APP_PROD_ENDPOINT : process.env.REACT_APP_DEV_ENDPOINT;
+const envType = "prod";
+const dev="http://localhost:8080/api/reservation/";
+const prod="https://iasd-reservation.azurewebsites.net/api/reservation/";
+
+export const reservationApi = envType === "prod" ? prod : dev;
 
